746b4bcea778880573a55db70ade3a040125faaf
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:16:06 -0300

    Add files via upload

---
+++ Soma_18_12_2020/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Soma_18_12_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, x;
+
+            Console.WriteLine();
+            Console.Write("Digite o primeiro número : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o segundo número : ");
+            b = int.Parse(Console.ReadLine());
+
+            x = a + b;
+
+            Console.WriteLine();
+            Console.Write("X = "+x);
+
+        }
+    }
+}
